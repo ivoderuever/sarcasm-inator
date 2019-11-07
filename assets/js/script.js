fc7b1f92c10e95ea6be27dd14a49eed4ca657f22
Ivo de Ruever <48962408+ivoderuever@users.noreply.github.com>
2019-11-08 00:17:02 +0100

    Added enter input for desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,47 @@
+var endword;
+
 function toggleHidden() {
     $("#word").val("");
     $("#input").toggleClass('hidden');
     $("#result").toggleClass('hidden');
 }
 
-var endword;
+function generateWord () {
+    var word = $("#word").val().toLowerCase();
+
+    if (word == "") {
+    } else {
+        var letters = word.split("");
+
+        var randomizeLenght = (letters.length / 2);
+
+        var minimum = 0;
+        var maximum = (letters.length - 1);
+
+        for (var i = 0; i <= randomizeLenght; i++) {
+            var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1) << 0) + minimum;
+            
+            letters[randomnumber] = "" + letters[randomnumber].toUpperCase();
+        }
+
+        $("#endword").text(letters.join(""));
+        toggleHidden();
+
+        endword = letters.join("");
+    }
+}
 
 $(document).ready(function () {
-    $("#process").click(function () {
-        var word = $("#word").val().toLowerCase();
-
-        if (word == "") {
-        } else {
-            var letters = word.split("");
-
-            var randomizeLenght = (letters.length / 2);
-
-            var minimum = 0;
-            var maximum = (letters.length - 1);
-
-            for (var i = 0; i <= randomizeLenght; i++) {
-                var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1) << 0) + minimum;
-                
-                letters[randomnumber] = "" + letters[randomnumber].toUpperCase();
-            }
-
-            $("#endword").text(letters.join(""));
-            toggleHidden();
-
-            endword = letters.join("");
+    $(document).keyup(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        
+        if (keycode == '13') {
+            generateWord();
         }
+    })
+    
+    $("#process").click(function () {
+        generateWord();
     })
 
     $("#tryAgain").click(function () {
@@ -54,6 +66,6 @@ $(document).ready(function () {
 
         setTimeout(function () {
             $('.messagebox').toggleClass('messagebox-open');
-        }, 700);
+        }, 800);
     });
 })
